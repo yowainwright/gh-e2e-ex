d31bacc0455916016529548afa9e06c68a3d5aca
Jeff Wainwright <yowainwright@gmail.com>
2023-07-24 17:52:59 -0700

    fix: adds basic repo test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test('basic repository test', async ({ page }) => {
+  await page.goto('https://github.com/yowainwright/gh-e2e-ex/');
+  const readmeTitle = page.locator('.markdown-body h1').first();
+  await expect(readmeTitle).toContainText('Github E2E Example');
 });
