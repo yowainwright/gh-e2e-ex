adc06d63dfcd700c1c64c09d7a12a25b42ddaa08
Jeff Wainwright <yowainwright@gmail.com>
2023-07-26 14:20:20 -0700

    fix: adds initial PR test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+const pullRequest = process.env.PR_NUMBER || undefined
+
 test('basic repository test', async ({ page }) => {
   await page.goto('https://github.com/yowainwright/gh-e2e-ex/');
   const readmeTitle = page.locator('.markdown-body h1').first();
   await expect(readmeTitle).toContainText('Github E2E Example');
 });
+
+test('basic PR test', async ({ page }) => {
+  const url = pullRequest ?
+    `https://github.com/yowainwright/gh-e2e-ex/pull/${pullRequest}` :
+    'https://github.com/yowainwright/gh-e2e-ex/pulls'
+  console.log('basic PR test', { pullRequest, url });
+  await page.goto(url);
+  await expect(page).toHaveURL(url);
+})
